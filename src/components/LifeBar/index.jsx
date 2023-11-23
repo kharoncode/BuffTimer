5f434026ce5d11f80ca080dae20cabaf544f9618
Remi Collin <sharon.code@proton.me>
2023-11-23 10:19:06 +0100

    add lifeBar

--- src/components/LifeBar/index.jsx
+++ src/components/LifeBar/index.jsx
@@ -1,11 +1,27 @@
-import styles from './lifeBar.module.css';
+import styled from 'styled-components';
+
+const LifeElt = styled.div`
+   font-weight: 500;
+   text-align: center;
+   color: black;
+   border: solid 1px black;
+   border-radius: 10px;
+   width: 80%;
+   height: 20px;
+   background-image: linear-gradient(
+      90deg,
+      rgba(102, 255, 87, 1) ${({ stop }) => stop}%,
+      rgba(255, 0, 83, 1) ${({ stop }) => stop}%,
+      rgba(255, 0, 83, 1) 100%
+   );
+`;
 
 export default function Life(life) {
    const lifeInfo = life.life;
-   const life_elt = document.getElementsByClassName(styles.container);
-   const stop = (lifeInfo.life / lifeInfo.maxLife) * 100;
-   if (life_elt[0]) {
-      life_elt[0].style.backgroundImage = `linear-gradient(90deg, rgba(60, 179, 113) ${stop}%, rgba(238, 130, 238) ${stop}%, rgba(238, 130, 238) 100%)`;
-   }
-   return <div className={styles.container}></div>;
+   const stop = Math.round((lifeInfo.now / lifeInfo.maxLife) * 100);
+   return (
+      <LifeElt stop={stop}>
+         {lifeInfo.now}/{lifeInfo.maxLife}
+      </LifeElt>
+   );
 }
