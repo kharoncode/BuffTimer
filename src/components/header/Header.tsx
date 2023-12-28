87a4c5b62fd6bbba78aca72da6330798a4b178ae
Collin Rémi <sharon.code@proton.me>
2023-12-28 14:55:50 +0100

    add logOut

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './header.module.css';
 import { useSelector } from 'react-redux';
 import { getAuth } from '@/router/selectors';
+import { store } from '@/router/store';
+import { loginSlice } from '@/pages/login/loginSlice';
 
 function Header() {
+   const navigate = useNavigate();
+
    const auth: boolean = useSelector(getAuth);
+
+   const logOut = () => {
+      store.dispatch(loginSlice.actions.resetLogin());
+      navigate('/login');
+   };
    //const profile: boolean = false;
    return (
       <header className={styles.header}>
@@ -18,9 +27,13 @@ function Header() {
             </div>
          </nav>
 
-         <div className={styles.settings}>
+         <div>
             {auth ? (
-               <div>Profile / LogOut</div>
+               <div className={styles.settings}>
+                  <Link to="/profile">Profile</Link>
+                  <span>/</span>
+                  <div onClick={() => logOut()}>LogOut</div>
+               </div>
             ) : (
                <Link to="/login">LogIn</Link>
             )}
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,9 @@
 import type { FormEvent, FunctionComponent } from 'react';
 import styles from './login.module.css';
-import { fetchProfile, loginSlice } from './loginSlice';
-import { useDispatch, useStore } from 'react-redux';
+import { fetchProfile } from './loginSlice';
+import { useDispatch } from 'react-redux';
 import { AppDispatch } from '@/router/store';
+import { useNavigate } from 'react-router-dom';
 
 export type loginData = {
    login: string;
@@ -10,31 +11,30 @@ export type loginData = {
 };
 
 export const Login: FunctionComponent = () => {
-   const store = useStore();
    const dispatch = useDispatch<AppDispatch>();
-
-   const logOut = () => {
-      store.dispatch(loginSlice.actions.resetLogin());
-   };
+   const navigate = useNavigate();
 
    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       const login: string = e.currentTarget.login.value;
       const password: string = e.currentTarget.password.value;
       const dataLog: loginData = { login: login, password: password };
-      dispatch(fetchProfile(dataLog));
+      dispatch(fetchProfile(dataLog)).then((data) => {
+         if (!data.error) {
+            navigate('/profile');
+         }
+      });
    };
 
    return (
       <main className={styles.main}>
-         <button onClick={logOut}>Reset</button>
          <div className={styles.container}>
             <h3>Login</h3>
             <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
                <label htmlFor="login"></label>
                <input type="text" id="login" name="login" />
                <label htmlFor="password"></label>
-               <input type="text" id="password" name="password" />
+               <input type="password" id="password" name="password" />
                <button type="submit">Connect</button>
             </form>
          </div>
