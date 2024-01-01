2fca7ac0872d42b1728bc942d2b26bfe0b8e9387
RC-Asus <sharon.code@proton.me>
2024-01-01 21:00:21 +0100

    fix key error

--- src/components/playerCard/PlayerCard.tsx
+++ src/components/playerCard/PlayerCard.tsx
@@ -2,21 +2,11 @@ import LifeBar from '@components/lifeBar/lifeBar';
 import SpellCard from '@components/spellCard/SpellCard';
 import styles from './playerCard.module.css';
 import type { player } from '@/utils/formatPlayer';
-//import EditPlayerCard from '../EditPlayerCard';
-//import { useState } from 'react';
-//import styled from 'styled-components';
-
-// const Modal = styled.div`
-//    width: 100%;
-//    justify-content: center;
-//    display: ${({ display }) => display};
-// `;
 
 type data = { player: player };
 
 const PlayerCard: React.FC<data> = (data: data) => {
    const { player } = data;
-   // const [isOpen, setIsOpen] = useState(false);
    return (
       <div id={`${player.id}Card`} className={styles.container}>
          <div className={styles.status}>
@@ -31,7 +21,7 @@ const PlayerCard: React.FC<data> = (data: data) => {
          <div className={styles.spellContainer}>
             {player.spells.map((el, index: number) =>
                el.date === null ? (
-                  <></>
+                  ''
                ) : (
                   <SpellCard
                      key={`${player.id}-${index}-spell`}
