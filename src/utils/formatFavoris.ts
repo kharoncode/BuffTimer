11fd6279d44c463cc6f91b27a40ebd04583e7cde
Collin Rémi <sharon.code@proton.me>
2024-01-01 16:17:44 +0100

    edit formatFavoris

--- src/utils/formatFavoris.ts
+++ src/utils/formatFavoris.ts
@@ -4,7 +4,6 @@ const formatFavoris = (list: string, players: players) => {
    const favoris: players = {};
    const favorisIds = list.split(' ');
    favorisIds.map((el) => (favoris[el] = players[el]));
-   console.log(players);
    return favoris;
 };
 
