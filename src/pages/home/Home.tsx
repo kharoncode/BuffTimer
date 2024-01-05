88d0f612e0ef4748b8f68695a60cef266298555d
Collin Rémi <sharon.code@proton.me>
2024-01-05 12:57:12 +0100

    add random playerCard on home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,99 +1,22 @@
 import styles from './home.module.css';
-import { store } from '@/router/store';
-import { playersSlice } from '@/pages/players/playersSlice';
 import PlayersContainer from '@/components/playersContainer/PlayersContainer';
+import { useSelector } from 'react-redux';
+import { getPlayersList } from '@/router/selectors';
+import { players } from '@/utils/formatPlayer';
 
 function Home() {
-   const reset = () => {
-      store.dispatch(playersSlice.actions.resetPlayers());
+   const randomProperty = function (obj: players) {
+      const keys = Object.keys(obj);
+      return obj[keys[(keys.length * Math.random()) << 0]];
    };
 
-   const players = {
-      almir: {
-         id: 'almir',
-         name: 'Almir',
-         picture:
-            'https://i.pinimg.com/originals/e3/1c/8b/e31c8b19dfc42750ad3655647f074a94.jpg',
-         life: {
-            currentLife: 157,
-            maxLife: 157,
-         },
-         spells: [
-            {
-               id: 'benedictionDeKeldar',
-               name: 'Bénédiction de Keldar',
-               category: 'justice',
-               date: 1704502738989,
-            },
-            {
-               id: 'attaqueSacree',
-               name: 'Attaque Sacrée',
-               category: 'justice',
-               date: null,
-            },
-            {
-               id: 'grandeBenedictionDeKeldar',
-               name: 'Grd Bénédiction de Keldar',
-               category: 'justice',
-               date: 1704854679482,
-            },
-            {
-               id: 'lameDeJustice',
-               name: 'Lame de Justice',
-               category: 'justice',
-               date: null,
-            },
-            {
-               id: 'transcendance',
-               name: 'Transcendance',
-               category: 'justice',
-               date: null,
-            },
-            {
-               id: 'regenerationMineure',
-               name: 'Régénération Mineure',
-               category: 'protection',
-               date: 1705566668827,
-            },
-            {
-               id: 'resistance',
-               name: 'Résistance',
-               category: 'protection',
-               date: null,
-            },
-            {
-               id: 'salutDuDivin',
-               name: 'Salut du Divin',
-               category: 'protection',
-               date: null,
-            },
-            {
-               id: 'regeneration',
-               name: 'Régénération',
-               category: 'protection',
-               date: 1704438358763,
-            },
-            {
-               id: 'capriceDuDestin',
-               name: 'Caprice du Destin',
-               category: 'protection',
-               date: 1705566668827,
-            },
-            {
-               id: 'chatiment',
-               name: 'Chatiment',
-               category: 'protection',
-               date: null,
-            },
-         ],
-      },
-   };
+   const players = useSelector(getPlayersList);
+   const player = randomProperty(players);
 
    return (
       <div className={styles.container}>
          Home
-         <PlayersContainer players={players} />
-         <button onClick={() => reset()}>Reset</button>
+         <PlayersContainer players={{ player }} />
       </div>
    );
 }
