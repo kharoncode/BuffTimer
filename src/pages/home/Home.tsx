edc02934b8901f73d9187f883efbae20ca9d60d6
Collin Rémi <sharon.code@proton.me>
2024-01-05 13:09:01 +0100

    init home tuto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -15,7 +15,36 @@ function Home() {
 
    return (
       <div className={styles.container}>
-         Home
+         <h2>Bienvenu sur BuffTimer</h2>
+         <section>
+            <article>
+               <h3>Qu'est-ce que "BuffTimer" ?</h3>
+               <p>
+                  C'est une application pour visualiser facilement quel Buff est
+                  actif et sur qui. Il permet aussi de connaître l'état de santé
+                  des joueurs ainsi que leurs besoins
+               </p>
+            </article>
+            <article>
+               <h3>Comment ça marche ?</h3>
+               <p>Rien de bien compiqué, je vous rassure !</p>
+               <ul>
+                  <h4>Le MENU :</h4>
+                  <li>BUFFTIMER : renvoit sur cette page</li>
+                  <li>Joueurs : affiche l'ensemble des joueurs</li>
+                  <li>
+                     Favoris : affiche les joueurs que vous avez ajouté dans vos
+                     favoris
+                  </li>
+                  <li>
+                     Info : permet aux joueurs de laisser un message sur leurs
+                     besoins
+                  </li>
+                  <li>Profile : vous permets de modifier votre profile</li>
+                  <li>LogOut : permet de se déconnecter </li>
+               </ul>
+            </article>
+         </section>
          <PlayersContainer players={{ player }} />
       </div>
    );
