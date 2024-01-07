3440270e9da97303c44e3fa9e03f29f39334cd25
RC-Asus <sharon.code@proton.me>
2024-01-07 23:11:10 +0100

    add edit button

--- src/components/playerCard/PlayerCard.tsx
+++ src/components/playerCard/PlayerCard.tsx
@@ -3,6 +3,7 @@ import styles from './playerCard.module.css';
 import type { player } from '@/utils/formatPlayer';
 import SpellsContainer from '../spellsContainer/SpellsContainer';
 import type { modale } from '@/pages/players/Players';
+import editIcone from '@assets/icones/edit.svg';
 
 type data = {
    player: player;
@@ -13,16 +14,19 @@ const PlayerCard: React.FC<data> = (data: data) => {
    const { player, setModale } = data;
    return (
       <div id={`${player.id}Card`} className={styles.container}>
-         <div
-            className={styles.status}
-            onClick={() => {
-               if (setModale) {
-                  setModale({ id: player.id, isOpen: true });
-               }
-            }}
-         >
+         <div className={styles.status}>
             <img
-               className={styles.img}
+               src={editIcone}
+               alt="Edit"
+               className={styles.editButton}
+               onClick={() => {
+                  if (setModale) {
+                     setModale({ id: player.id, isOpen: true });
+                  }
+               }}
+            />
+            <img
+               className={styles.playerPicture}
                src={player.picture}
                alt={player.name}
             ></img>
