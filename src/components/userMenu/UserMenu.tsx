f9bd37acf7a748f47f9328b34293e01d2fd80950
RC-Asus <sharon.code@proton.me>
2024-01-08 23:09:38 +0100

    add reload data button in user profile

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -5,6 +5,9 @@ import EditUser from './EditUser';
 import styles from './userMenu.module.css';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import closeIcone from '@assets/icones/close.svg';
+import { useDispatch } from 'react-redux';
+import { fetchPlayers } from '@/pages/players/playersSlice';
+import { AppDispatch } from '@/router/store';
 
 const Section = (props: { section: string | undefined }) => {
    const { section } = props;
@@ -37,6 +40,7 @@ const Section = (props: { section: string | undefined }) => {
 export const UserMenu = () => {
    const { section } = useParams();
    const navigate = useNavigate();
+   const dispatch = useDispatch<AppDispatch>();
    return section === 'menu' ? (
       <div className={styles.container}>
          <button
@@ -62,6 +66,14 @@ export const UserMenu = () => {
             onClick={() => navigate('/user/editPassword')}
          >
             Changer le Mot de Passe
+         </button>
+         <button
+            className={styles.buttonReload}
+            onClick={() => {
+               dispatch(fetchPlayers());
+            }}
+         >
+            Recharger les données des joueurs
          </button>
       </div>
    ) : (
