39ceda2ac6827dd82b27142f8587f6aaf6c95745
Collin Rémi <sharon.code@proton.me>
2024-01-09 09:29:35 +0100

    edit home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,41 +1,19 @@
 import styles from './home.module.css';
-//import PlayersContainer from '@/components/playersContainer/PlayersContainer';
-import { useSelector } from 'react-redux';
-import { getDataPages } from '@/router/selectors';
-//import { getAuth, getPlayersList } from '@/router/selectors';
-//import { players } from '@/utils/formatPlayer';
-
-type select = {
-   home: {
-      title: string;
-      articles: { [key: string]: { [key: string]: string } };
-   };
-};
 
 function Home() {
-   //const auth = useSelector(getAuth);
-   /* const randomProperty = function (obj: players) {
-      const keys = Object.keys(obj);
-      return obj[keys[(keys.length * Math.random()) << 0]];
-   }; */
-
-   /* const players = useSelector(getPlayersList);
-   const player = randomProperty(players); */
-
-   const { home }: select = useSelector(getDataPages);
-
    return (
       <div className={styles.container}>
-         <h2>{home.title}</h2>
+         <h2>Bienvenu sur BuffTimer</h2>
          <section className={styles.section}>
-            {Object.values(home.articles).map((el) => (
-               <article className={styles.article} key={el.id}>
-                  <h3>{el.title}</h3>
-                  <p>{el.article}</p>
-               </article>
-            ))}
+            <article className={styles.article}>
+               <h3>Qu'est-ce que 'BuffTimer' ?</h3>
+               <p>
+                  "C'est une application pour visualiser facilement quel Buff
+                  est actif et sur qui. Il permet aussi de connaître l'état de
+                  santé des joueurs ainsi que leurs besoins."
+               </p>
+            </article>
          </section>
-         {/* {auth ? <PlayersContainer players={{ player }} /> : <></>} */}
       </div>
    );
 }
