1e84660d96a5af7172085d6c87706e945c8ee898
Collin Rémi <sharon.code@proton.me>
2024-01-09 16:14:51 +0100

    clean code

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -4,7 +4,6 @@ import Home from '@/pages/home/Home';
 import { Login } from '@/pages/login/Login';
 import Favoris from '@/pages/favoris/Favoris';
 import Players from '@/pages/players/Players';
-//import Error from '@/pages/error/Error';
 import { persistor, store } from './store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -38,7 +37,6 @@ function App() {
                      <Route path="/players" element={<Players />} />
                      <Route path="/info" element={<Info />} />
                   </Route>
-                  {/* <Route path="*" element={<Error />} /> */}
                </Routes>
             </HashRouter>
          </PersistGate>
